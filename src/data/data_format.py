53ee9f028b7484546bede722eb21bf8133af8789
mtamine <melissa.tamine@ensae.fr>
2023-03-10 15:03:40 +0000

    format data and add useful functions

---
+++ src/data/data_format.py
@@ -0,0 +1,122 @@
+from typing import Dict, List, Any, OrderedDict, Union
+import collections
+import random
+import json
+from tqdm import tqdm
+
+
+def ordered_dict(obj: dict) -> Union[OrderedDict, object]:
+    if type(obj) == 'dict':
+        return collections.OrderedDict(obj)
+    else:
+        return obj
+
+class Utterance:
+    def __init__(self, 
+                 text: str = None,
+                 identifier: str = None,
+                 da: str = None,
+                 sentiment: str = None,
+                 label : int = None,
+                 meta: Dict[str, Any] = None):
+        self.text = text
+        self.identifier = identifier
+        self.da = da
+        self.sentiment = sentiment
+        self.label = label
+        self.meta = meta
+
+    def to_dict(self) -> Dict:
+        res = {}
+        if self.text:
+            res.update({'text': self.text})         
+        if self.identifier:
+            res.update({'identifier': self.identifier})
+        if self.da:
+            res.update({'da': self.da})
+        if self.sentiment:
+            res.update({'sentiment': self.sentiment})
+        if self.label:
+            res.update({'label': self.label})
+        if self.meta:
+            res.update({'meta': self.meta})
+        return res
+    
+    def from_dict(self, d: Dict):
+        text = self.text
+        identifier = self.identifier
+        da = self.da
+        sentiment = self.sentiment
+        label = self.label
+        meta = self.meta
+        if 'text' in d.keys():
+            text = d['text']
+        if 'identifier' in d.keys():
+            identifier = d['identifier']
+        if 'da' in d.keys():
+            da = d['da']
+        if 'sentiment' in d.keys():
+            sentiment = d['sentiment']
+        if 'label' in d.keys():
+            label = d['label']
+        if 'meta' in d.keys():
+            meta = d['meta']
+
+        return Utterance(text = text, identifier = identifier, da = da, sentiment = sentiment, label = label, meta = meta)
+
+
+class UtteranceCollection:
+    def __init__(self, utterances: List[Utterance] = None,
+                 meta: Dict[str, Any] = None):
+        """
+        :param utterances: a list of Utterances objects
+        :param meta: the meta information associated with the collection
+        """
+        self.utterances = utterances
+        self.meta = meta
+
+    def __len__(self):
+        return len(self.utterances)
+
+    def __getitem__(self, index):
+        return self.utterances[index]
+
+    def random_subset(self, k:int) -> List[Utterance]:
+        """
+        Return a random subset of length k of the collection of utterances
+        """
+        indices = random.choices(range(0, len(self.sentences)), k=k)
+        res = []
+        for i in indices:
+            res.append(self.utterances[i])
+        return res
+    
+    def to_dict(self) -> Dict:
+        res = {}
+        if self.utterances:
+            utterances = []
+            for utterance in self.utterances:
+                utterances.append(utterance.to_dict())
+            res.update({'utterances': utterances})
+        if self.meta:
+            res.update({'meta': self.meta})
+        return res
+
+    def save(self, output_path: str):
+        print(f'####### Save utterance collection to {output_path} #########')
+        with open(output_path, 'w', encoding = 'utf8') as out:
+            json.dump(self.to_dict(), out, ensure_ascii = False, indent = None)
+    
+    def load_from_json(self, input_filename: str):
+        with open(input_filename, 'r', encoding='utf8') as f:
+            d = json.load(f)
+        if 'meta' in d.keys():
+            meta = d['meta']
+        utterances = d['utterances']
+        stcs = []
+        print("############# Loading utterances from json file #################")
+        for utterance in tqdm(utterances):
+            u = Utterance()
+            u = u.from_dict(d = utterance)
+            utterances.append(u)
+        return UtteranceCollection(utterances = utterances, meta = meta)
--- src/data/utils.py
+++ src/data/utils.py
@@ -1,5 +1,47 @@
-import torch
-from config.config import config
+import uuid
+from tqdm import tqdm
+from typing import List
+from src.data.data_format import *
 
-def get_device():
-    return torch.device(f'cuda:{config.CUDA_DEVICE}' if torch.cuda.is_available() else 'cpu')
+def convert_transformers_dataset_to_passages(dataset) -> List[Utterance]:
+    """
+    This function converts a Transformer Dataset into a list of Utterance objects
+    """
+    print('######## Converting dataset into a list of utterances ########')
+    utterances = []
+    for i in tqdm(range(len(dataset))):
+        text = dataset[i]['Utterance']
+        identifier = uuid.uuid1().hex
+        label = dataset[i]['Label']
+        meta = dataset[i]
+        meta.pop('Utterance')
+        meta.pop('Label')
+        if 'Dialogue_Act' in dataset[i].keys():
+            da = dataset[i]['Dialogue_Act']
+            meta.pop('Dialogue_Act')
+            utterance = Utterance(text = text, identifier = identifier, da = da, label = label, meta = meta)
+        if 'Sentiment' in dataset[i].keys():
+            sentiment = dataset[i]['Sentiment']
+            meta.pop('Sentiment')
+            utterance = Utterance(text = text, identifier=identifier, sentiment = sentiment, label = label, meta = meta)
+        utterances.append(utterance)
+    print(f"################ {len(dataset['Utterance'])} formated #################")
+    return utterances
+
+def generate_batches(input: List[object], batch_size) -> List[object]:
+    for i in tqdm(range(0, len(input), batch_size)):
+        yield input[i:i+batch_size]
+
+    
+def split_into_train_test_val_sets(utterances: List[Utterance], train_ratio: float = 0.7, test_ratio: float=0.1) -> dict:
+    total = len(utterances)
+    train_count = int(train_ratio * total)
+    test_count = int(test_ratio * total)
+    if total - train_count - test_count > 0:
+        random.shuffle(utterances)
+        train_utterances = utterances[0:train_count]
+        test_utterances = utterances[train_count:train_count + test_count]
+        val_utterances = utterances[train_count + test_count:]
+        return {'train': train_utterances, 'test': test_utterances, 'val': val_utterances}
+    else:
+        raise Exception('Set correct ratios')
