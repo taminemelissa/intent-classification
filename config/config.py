8126de7c69d0b2615af4ace8e2b88884efc62bf4
mtamine <melissa.tamine@ensae.fr>
2023-03-13 13:33:45 +0000

    example of config file

--- config/config.py
+++ config/config.py
@@ -1,2 +1,21 @@
-#DEVICE
+# DEVICE
 CUDA_DEVICE = 0
+
+# PARAMETERS
+SEED = 42
+VERSION = 1
+FILENAME = f'bert-base-uncased-v{VERSION}'
+
+#PATHS
+HOME = '/home/onyxia/work/intent-classification'
+MODELS = f'{HOME}/models'
+DIR_CHECKPOINTS = f'{HOME}/checkpoints'
+LOGS = f'{HOME}/logs'
+
+# DATA
+DATASETNAME = 'dyda_da'
+
+# HYPERPARAMETERS
+BATCH_SIZE = 12
+LEARNING_RATE = 0.0001
+NUM_EPOCHS = 50
